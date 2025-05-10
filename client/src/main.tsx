0972b26faa3ebc6f5c424d5c43e4b00871526b2c
Alexander Cherkasov <alkorotitskij@gmail.com>
2025-05-11 00:11:23 +0300

    Создать базовый роутинг

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,4 +1,10 @@
+import { router } from '@app/router'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { RouterProvider } from 'react-router'
 
-createRoot(document.getElementById('root')!).render(<StrictMode></StrictMode>)
+createRoot(document.getElementById('root')!).render(
+	<StrictMode>
+		<RouterProvider router={router}></RouterProvider>
+	</StrictMode>
+)
